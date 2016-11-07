e75525483d801297f1ea8d6bf5661f6243c619e4
Stefan Arentz <stefan@arentz.ca>
2016-11-07 18:57:28 -0500

    Bug 1315784 - Reorganize L10N Screenshot Tests

--- L10nSnapshotTests/L10nPermissionStringsSnapshotTests.swift
+++ L10nSnapshotTests/L10nPermissionStringsSnapshotTests.swift
@@ -1,9 +1,20 @@
-//
-//  L10nPermissionStringsSnapshotTests.swift
-//  Client
-//
-//  Created by Stefan Arentz on 2016-11-07.
-//  Copyright © 2016 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Foundation
+import XCTest
+
+class L10nPermissionStringsSnapshotTests: L10nBaseSnapshotTests {
+    func testNSLocationWhenInUseUsageDescription() {
+        loadWebPage("http://people.mozilla.org/~sarentz/fxios/testpages/geolocation.html", waitForOtherElementWithAriaLabel: "body")
+        snapshot("PermissionStrings-Location")
+    }
+
+    func testNSPhotoLibraryUsageDescription() {
+        // TODO
+    }
+
+    func testNSCameraUsageDescription() {
+        // TODO
+    }
+}
